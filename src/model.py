67e38974a4496d54fc83ab4eb6a3aa36fa590543
Komales-code <s23b0203@siswa.umk.edu.my>
2026-01-03 16:04:57 +0800

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,10 +4,7 @@ from sklearn.metrics import mean_squared_error
 
 def evaluate_svr(params, X_train, X_test, y_train, y_test):
     C, epsilon, gamma = params
-
     model = SVR(C=C, epsilon=epsilon, gamma=gamma)
     model.fit(X_train, y_train)
-    predictions = model.predict(X_test)
-
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-    return rmse
+    preds = model.predict(X_test)
+    return np.sqrt(mean_squared_error(y_test, preds))
